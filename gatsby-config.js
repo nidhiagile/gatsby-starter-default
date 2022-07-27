99b9e43bb77d28ff888411e3f76dbb62f5ace6f0
nidhiagile <110100905+nidhiagile@users.noreply.github.com>
2022-07-27 16:44:01 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    resolve: `gatsby-source-wordpress`,
+      options: {
+        // the only required plugin option for WordPress is the GraphQL url.
+        url:
+          process.env.WPGRAPHQL_URL ||
+          `http://192.168.2.116/wordpress_agile_website/graphql`,
+      },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
